4e8a13c44f7037ebca3d6df2611face555e0f9d6
moehzi <faizi1234098@gmail.com>
2020-12-29 00:47:38 +0800

    navbar and add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+	colors:{
+		'primary' : '#FF9376',
+		'secondary':'#8080FF',
+		'black':'#000000',
+		gray:{
+			light:'#999999'
+		}
+	},
 	fontFamily: {
 		body:['Poppins']
 	   },
